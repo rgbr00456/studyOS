29973718b2ee16502de4d7db917770800804e075
Ricardo Pedrosa Ramos Filho <97986958+rgbr00456@users.noreply.github.com>
2026-03-19 12:01:29 -0300

    Fix: vercel.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
   plugins: [svelte()],
-  // App lives at /app on the deployed site
-  base: '/app',
   build: {
     target: 'es2020',
     outDir: 'dist'
